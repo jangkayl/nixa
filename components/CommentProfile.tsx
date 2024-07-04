79949db6613ec1e0cf4bcbac56ecd84cf01dce20
jangkayl <villarazojk@gmail.com>
2024-07-04 18:14:21 +0800

    no comments

--- components/CommentProfile.tsx
+++ components/CommentProfile.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ComProfile from "./ProfileComments/ComProfile";
 import ComComProfile from "./ProfileComments/ComComProfile";
+import { BsEmojiGrin } from "react-icons/bs";
 
 const CommentProfile = ({ username, comments }: any) => {
 	const [filteredComments, setFilteredComments] = useState<any[]>([]);
@@ -17,32 +18,40 @@ const CommentProfile = ({ username, comments }: any) => {
 	if (!comments) return null;
 	return (
 		<div>
-			{filteredComments.map((com: any) => {
-				let countPost: any[] = [];
-				return (
-					<div key={com.id}>
-						{com.comments.map((reply: any) => (
-							<div key={reply.id}>
-								{reply.username === username && !countPost.includes(com.id) && (
-									<div>
-										<div>
-											<ComProfile
-												post={com}
-												id={com.id}
-											/>
-											<ComComProfile
-												id={com.id}
-												reply={reply.username}
-											/>
-											<div className="hidden">{countPost.push(com.id)}</div>
-										</div>
-									</div>
-								)}
-							</div>
-						))}
-					</div>
-				);
-			})}
+			{filteredComments.length < 1 ? (
+				<div className="flex flex-col justify-center items-center gap-3 h-[7rem]">
+					<BsEmojiGrin size={30} />
+					<p>No comments yet</p>
+				</div>
+			) : (
+				filteredComments.map((com: any) => {
+					let countPost: any[] = [];
+					return (
+						<div key={com.id}>
+							{com.comments.map((reply: any) => (
+								<div key={reply.id}>
+									{reply.username === username &&
+										!countPost.includes(com.id) && (
+											<div>
+												<div>
+													<ComProfile
+														post={com}
+														id={com.id}
+													/>
+													<ComComProfile
+														id={com.id}
+														reply={reply.username}
+													/>
+													<div className="hidden">{countPost.push(com.id)}</div>
+												</div>
+											</div>
+										)}
+								</div>
+							))}
+						</div>
+					);
+				})
+			)}
 		</div>
 	);
 };
